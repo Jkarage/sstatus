44ff5b253e571e6b3af878169c636d41d6a435ca
Jkarage <josephbkarage@gmail.com>
2023-07-31 15:05:53 +0300

    Updated cpu displayment

--- handlers.go
+++ handlers.go
@@ -20,13 +20,19 @@ func memstats(c *fiber.Ctx) error {
 }
 
 func cpustats(c *fiber.Ctx) error {
-	t, i, err := getCPUStats()
-	if err != nil {
-		return c.Status(http.StatusInternalServerError).JSON("Couldn't read cpu details")
-	}
+	// t, i, err := getCPUStats()
+	// if err != nil {
+	// 	return c.Status(http.StatusInternalServerError).JSON("Couldn't read cpu details")
+	// }
 
-	usage := 1 - (i / (t + i))
+	// usage := 1 - (i / (t + i))
+	// return c.Status(http.StatusOK).JSON(fiber.Map{
+	// 	"CPU usage": usage * 100,
+	// })
+
+	cpuUsage := getCPUPercentage()
 	return c.Status(http.StatusOK).JSON(fiber.Map{
-		"CPU usage": usage * 100,
+		"CPU usage": cpuUsage,
 	})
+
 }
